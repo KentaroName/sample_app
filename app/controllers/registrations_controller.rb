88bc18e942fe8415bbae4b02ac435ddd6084e2f3
EC2 Default User <ec2-user@ip-172-31-21-139.ec2.internal>
2018-07-23 13:33:13 +0000

    Finish sign in

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,9 +3,9 @@ class RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(resource)
     user_path(resource)
   end
-
+ 
   private
-
+ 
       def sign_up_params
         params.require(:user).permit(:name, :email, :password, :password_confirmation)
       end
