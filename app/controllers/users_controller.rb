99b4c93f8f48201c3042f1eaa34c9dfb135ab7a0
EC2 Default User <ec2-user@ip-172-31-21-139.ec2.internal>
2018-07-25 23:43:16 +0000

    Finish user index action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, :except=>[:show]
+  def index
+      @users = User.paginate(page: params[:page])
+  end
+
   def show
       @user = User.find(params[:id])
+      #Showアクション＝ユーザー情報を表示するアクション
+      
       #データを渡すところ
       #show.html.erbの
       #<%= @user.name %>, <%= @user.email %>
       #を引っ張ってくる！！！！！
       #こいつでShowに示した内容を取り出すことに成功！！！！！！！！
+      
+      
   end
 end
